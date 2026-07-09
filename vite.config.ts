aad7063a4df9f65c66b39ccd21b7b9fe261ccc04
joako-18 <joako1327@gmail.com>
2026-07-09 12:27:51 -0600

    sin ofuscacion

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,26 @@ import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator'
 export default defineConfig({
   plugins: [
     react(),
-    obfuscatorPlugin({
-      include: ['src/**/*.js', 'src/**/*.ts', 'src/**/*.jsx', 'src/**/*.tsx'],
-      exclude: [/node_modules/],
-      apply: 'build',
-      debugger: true,
-      options: {
-        compact: true,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        identifierNamesGenerator: 'hexadecimal',
-        numbersToExpressions: true,
-        simplify: true,
-        splitStrings: true,
-        splitStringsChunkLength: 10,
-        stringArray: true,
-        stringArrayCallsTransform: true,
-        stringArrayCallsTransformThreshold: 0.5,
-        stringArrayEncoding: ['base64', 'rc4'],
-        stringArrayThreshold: 0.75,
-      }
-    })
+// obfuscatorPlugin({
+    //   include: ['src/**/*.js', 'src/**/*.ts', 'src/**/*.jsx', 'src/**/*.tsx'],
+    //   exclude: [/node_modules/],
+    //   apply: 'build',
+    //   debugger: true,
+    //   options: {
+    //     compact: true,
+    //     deadCodeInjection: true,
+    //     deadCodeInjectionThreshold: 0.4,
+    //     identifierNamesGenerator: 'hexadecimal',
+    //     numbersToExpressions: true,
+    //     simplify: true,
+    //     splitStrings: true,
+    //     splitStringsChunkLength: 10,
+    //     stringArray: true,
+    //     stringArrayCallsTransform: true,
+    //     stringArrayCallsTransformThreshold: 0.5,
+    //     stringArrayEncoding: ['base64', 'rc4'],
+    //     stringArrayThreshold: 0.75,
+    //   }
+    // })
   ],
 })
